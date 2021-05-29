bc5e091a6336e537de85f2ba5599075ed552cd7d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2021-05-29 13:56:22 -0700

    basic file to test that typescript stuff works in ide

---
+++ client/app.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const AppFrame = () => (
+  <>
+    <CssBaseline>
+      <h1>Something</h1>
+    </CssBaseline>
+  </>
+);
+
+export default AppFrame;
