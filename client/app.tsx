be7fde9cf71115d22d28375f3bbfaf0040b8deb0
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-01-27 10:27:32 -0800

    added theme provided to prepare for mui upgrade

--- client/app.tsx
+++ client/app.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+
+const theme = createTheme();
 
 function AppFrame() {
   return (
-    <CssBaseline>
-      <h1>Something</h1>
-    </CssBaseline>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <h1>Something</h1>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
