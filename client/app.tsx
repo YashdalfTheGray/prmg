dea93e74d5374d4d6804f853d2e8c979e14ede04
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-01-28 09:06:26 -0800

    material ui 4 to 5 migration changes

--- client/app.tsx
+++ client/app.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
 
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import {
+  StyledEngineProvider,
+  Theme,
+  ThemeProvider,
+  createTheme,
+} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import '@mui/styles';
+
+declare module '@mui/styles/defaultTheme' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface DefaultTheme extends Theme {}
+}
 
 const theme = createTheme();
 
 function AppFrame() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <h1>Something</h1>
-      </CssBaseline>
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <h1>Something</h1>
+        </CssBaseline>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
